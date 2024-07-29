bf27b49e498452565f3ef2081380f1e6ff07ca1a
Fengchang Lv <fengchang.lv@shopee.com>
2024-07-29 09:31:21 +0800

    update

--- cookbook/02-API/AuxStream/main.cc
+++ cookbook/02-API/AuxStream/main.cc
@@ -13,6 +13,10 @@ const std::string trtFile {"./model.plan"};
 static Logger gLogger(ILogger::Severity::kVERBOSE);
 
 int main() {
+    std::random_device rd;
+    std::mt19937 gen(rd);
+    std::uniform_real_distribution<> dis(-1.0, 1.0);
+
     IBuilder *builder = createInferBuilder(gLogger);
     INetworkDefinition * network = builder->createNetworkV2(1 << int(NetworkDefinitionCreationFlag::kEXPLICIT_BATCH));
     IOptimizationProfile *profile = builder->createOptimizationProfile();
@@ -61,17 +65,95 @@ int main() {
         return -1;
     }
 
-    std::ofstream engineFile(trtFile, std::ios::binary);
-    if (!engineFile) {
-        std::cout << "failed open file to write" << std::endl;
-        return -1;
+    auto nIO = engine->getNbIOTensors();
+    std::vector<std::string> vTensorName(nIO);
+    int nInput = 0;
+    int nOutput = 0;
+
+    for (int i = 0; i < nIO; i++) {
+        vTensorName[i] = engine->getIOTensorName(i);
+        nInput += (engine->getTensorIOMode(vTensorName[i].c_str()) == TensorIOMode::kINPUT);
+        nOutput += int(engine->getTensorIOMode(vTensorName[i].c_str()) == TensorIOMode::kOUTPUT);
     }
-    engineFile.write(static_cast<char *>(engineString->data()), engineString->size());
-    if (engineFile.fail()) {
-        std::cout << "failed saving plan file" << std::endl;
+
+    IExecutionContext *context = engine->createExecutionContext();
+    if (context == nullptr) {
+        std::cout << "create exeuction context failed" << std::endl;
         return -1;
     }
 
+    for (int i = 0; i < nInput; i++) {
+        context->setInputShape(vTensorName[i].c_str(), Dims32 {4, {4, 4, nMKN, nMKN}});
+    }
+
+    for (int i = 0; i < nIO; ++i) {
+        std::cout << std::string(i < nInput ? "Input [" : "Output [");
+        std::cout << i << std::string("]-> ");
+        std::cout << dataTypeToString(engine->getTensorDataType(vTensorName[i].c_str())) << std::string(" ");
+        std::cout << shapeToString(engine->getTensorShape(vTensorName[i].c_str())) << std::string(" ");
+        std::cout << shapeToString(context->getTensorShape(vTensorName[i].c_str())) << std::string(" ");
+        std::cout << vTensorName[i] << std::endl;
+    }
+
+    int nAuxStream = engine->getNbAuxStreams();
+    std::vector<cudaStream_t> vCudaStream(nAuxStream);
+    for (int i = 0; i < nAuxStream; i++) {
+        auto err = cudaStreamCreate(&vCudaStream[i]);
+        if (err != cudaSuccess) {
+            std::cout << "cuda stream create err = " << err << std::endl;
+            return -1;
+        }    
+    }
+    context->setAuxStreams(vCudaStream.data(), vCudaStream.size());
+
+    std::vector<int> vTensorSize(nIO, 0);
+    for (int i = 0; i < nIO; i++) {
+        Dims32 dim = context->getTensorShape(vTensorName[i].c_str());
+        int size = 1;
+        for (int j = 0; j < dim.nbDims; j++) {
+            size *= dim.d[j];
+        }
+        vTensorSize[i] = size * dataTypeToSize(engine->getTensorDataType(vTensorName[i].c_str()));
+    }
+
+    for (auto size : vTensorSize) {
+        std::cout << "tensor size = " << size << std::endl;
+    }
+
+    std::vector<void *> vBufferH {nIO, nullptr};
+    std::vector<void *> vBufferD {nIO, nullptr};
+    for (int i = 0; i <  nIO; i++) {
+        vBufferH[i] = static_cast<void *>(new char[vTensorSize[i]]]);
+        CHECK(cudaMalloc(&vBufferD[i], vTensorSize[i]));
+    }
+
+    // random generate input between [-1, 1]
+    for (int i = 0; i < nInput; i++) {
+        float *pData = static_cast<float*>(vBufferH[i])
+        size_t typeSize = dataTypeToSize(engine->getTensorDataType(vTensorName[i].c_str()));
+        for (int j = 0; j < vTensorSize[i] / typeSize; j++) {
+            pData[j] = static_cast<float>(dis(gen))
+        }
+
+    }
+
+    for (int i = 0; i < nInput; i++) {
+        CHECK(cudaMemcpy(vBufferD[i], vBufferH[i], vTensorSize[i], cudaMemcpyHostToDevice));
+    }
+
+    for (int i = 0; i < nIO; i++) {
+        context->setTensorAddress(vTensorName[i], vBufferD[i]);
+    }
+
+    context->enqueueV(0);
+
+    for (int i = nInput; i < nIO; i++) {
+        CHECK(cudaMemcpy(vBufferH[i], vBufferD[i], vTensorName[i], cudaMemcpyDeviceToHost));
+    }
+
+    for (int i = nInput; i < nIO; i++) {
+        printArrayInfo(static_cast<float*>(vBufferH[i]), context->getTensorShape(vTensorName[i].c_str()), vTensorName[i], true, true);
+    }
 
     return 0;
 }
--- cookbook/02-API/common.h
+++ cookbook/02-API/common.h
@@ -1,6 +1,10 @@
 #include <NvInfer.h>
 #include <fstream>
 #include <iostream>
+#include <random>
+#include <cuda_runtime.h>
+#include <cuda_runtime_api.h>
+
 
 using namespace nvinfer1;
 
@@ -87,3 +91,109 @@ __inline__ std::string dataTypeToString(DataType dataType)
         return std::string("Unknown");
     }
 }
+
+size_t dataTypeToSize(DataType dataType) {
+    switch (dataType) {
+        case DataType::kFLOAT:
+            return 4;
+        case DataType::kHALF:
+            return 2;
+        case DataType::kINT8:
+            return 1;
+        case DataType::kINT32:
+            return 4;
+        case Dataype::kBOOL:
+            return 1;
+        case DataType::kUINT8:
+            return 1;
+        case DataType::kFP8:
+            return 1;
+        default:
+            return 4;
+    }
+}
+
+template <typename T>
+void printArrayRecursively(const T *pArray, Dims32 dim, int iDim, int iStart) {
+    if (iDim == dim.nbDims - 1) {
+        for (int i = 0; i < dim.d[iDim]; ++i) {
+            std::cout << std::fixed << std::setprecision(3) << std::setw(6) << double(pArray[iStart + i]) << " ";
+        }
+    } else {
+        int nElem = 1;
+        for (int i = iDim + 1; i < dim.nbDims; ++i) {
+            nElem *= dim.d[i];
+        }
+        for (int i = 0; i < dim.d[iDim]; ++i) {
+            printArrayRecursively<T>(pArray, dim, iDim + 1, iStart + i * nElem)
+        }
+    }
+    std::cout << std::endl;
+}
+
+template <typeName T>
+void printArrayInfo(const T *pArray, Dims32 dim, std::string name = "", bool bPrintInfo = true, bool bPrintArrary = false, int n = 10){
+    std::cout << std::endl;
+    std::cout << name << ": (";
+    for (int i = 0; i < dim.nbDims; i++) {
+        std::cout << dim.d[i] << ", ";
+    }
+    std::cout << ")" << std::endl;
+
+    // print information
+    if (bPrintInfo) {
+        int nElement = 1; // number of elements with batch dimension
+        for (int i = 0; i < dim.nbDims; ++i)
+        {
+            nElement *= dim.d[i];
+        }
+
+        double sum      = double(pArray[0]);
+        double absSum   = double(fabs(double(pArray[0])));
+        double sum2     = double(pArray[0]) * double(pArray[0]);
+        double diff     = 0.0;
+        double maxValue = double(pArray[0]);
+        double minValue = double(pArray[0]);
+        for (int i = 1; i < nElement; ++i)
+        {
+            sum += double(pArray[i]);
+            absSum += double(fabs(double(pArray[i])));
+            sum2 += double(pArray[i]) * double(pArray[i]);
+            maxValue = double(pArray[i]) > maxValue ? double(pArray[i]) : maxValue;
+            minValue = double(pArray[i]) < minValue ? double(pArray[i]) : minValue;
+            diff += abs(double(pArray[i]) - double(pArray[i - 1]));
+        }
+        double mean = sum / nElement;
+        double var  = sum2 / nElement - mean * mean;
+
+        std::cout << "absSum=" << std::fixed << std::setprecision(4) << std::setw(7) << absSum << ",";
+        std::cout << "mean=" << std::fixed << std::setprecision(4) << std::setw(7) << mean << ",";
+        std::cout << "var=" << std::fixed << std::setprecision(4) << std::setw(7) << var << ",";
+        std::cout << "max=" << std::fixed << std::setprecision(4) << std::setw(7) << maxValue << ",";
+        std::cout << "min=" << std::fixed << std::setprecision(4) << std::setw(7) << minValue << ",";
+        std::cout << "diff=" << std::fixed << std::setprecision(4) << std::setw(7) << diff << ",";
+        std::cout << std::endl;
+
+               // print first n element and last n element
+        for (int i = 0; i < n; ++i)
+        {
+            std::cout << std::fixed << std::setprecision(5) << std::setw(8) << double(pArray[i]) << ", ";
+        }
+        std::cout << std::endl;
+        for (int i = nElement - n; i < nElement; ++i)
+        {
+            std::cout << std::fixed << std::setprecision(5) << std::setw(8) << double(pArray[i]) << ", ";
+        }
+        std::cout << std::endl;
+    }    
+
+    if (bPrintArrary) {
+        printArrayRecursively<T>(pArray, dim, 0, 0);
+    }
+}
+
+template void printArrayInfo(const float *, Dims32, std::string, bool, bool, int);
+template void printArrayInfo(const half *, Dims32, std::string, bool, bool, int);
+template void printArrayInfo(const char *, Dims32, std::string, bool, bool, int);
+template void printArrayInfo(const int *, Dims32, std::string, bool, bool, int);
+template void printArrayInfo(const bool *, Dims32, std::string, bool, bool, int);
