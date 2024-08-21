15c93a9d42c329ba607b430f0468d5d41d0cf0d3
fclv <479704553@qq.com>
2024-08-21 21:55:07 +0800

    add builder cc

--- cookbook/02-API/Builder/main.cc
+++ cookbook/02-API/Builder/main.cc
@@ -13,7 +13,37 @@ int main() {
     IIdentityLayer *identityLayer = network->addIdentity(input);
     network->markOutput(identityLayer->getOutput(0));
 
-    
+    bool succ = builder->setMaxThreads(int32_t(5));
+    if (succ) {
+        std::cout << "set max threads success" << std::endl;
+    } else {
+        std::cout << "set max threads failed" << std::endl;
+    }
+
+    std::cout << "max threads = " << builder->getMaxThreads() << std::endl;
+    if (builder->isNetworkSupported(network, config)) {
+        std::cout << "network is suppored" << std::endl;
+    } else {
+        std::cout << "network is not supported" << std::endl;
+    }
+    if (builder->platformHasFastFp16()) {
+        std::cout << "has fast fp16" << std::endl;
+    } else {
+        std::cout << "not have fast fp16" << std::endl;
+    }
+    if (builder->platformHasFastInt8()) {
+        std::cout << "has fast int8" << std::endl;
+    } else {
+        std::cout << "not have fast int8" << std::endl;
+    }
+    if (builder->platformHasTf32()) {
+        std::cout << "has tf32" << std::endl;
+    } else {
+        std::cout << "not have tf32" << std::endl;
+    }
+
+    IHostMemory *engineString = builder->buildSerializedNetwork(network, config)
+
 
 
     return 0;
